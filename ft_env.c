9cd56fb764896fb600e3eef2f9a4c0f554eb7492
soko <soko@gmail.com>
2024-11-04 03:54:33 +0100

    ssdfsDF

--- ft_env.c
+++ ft_env.c
@@ -58,6 +58,7 @@ void ft_backup(t_env **head, t_minishell data)
 void ft_env(t_minishell data, t_env *expo)
 {
     int i = 0;
+    (void)expo;
     t_env *head = NULL;
     t_env *last_node = NULL;
     
@@ -84,84 +85,4 @@ void ft_env(t_minishell data, t_env *expo)
         last_node = cmd_env;
         i++;
     }
-    if (expo != NULL)
-    {
-        printf("%s=%s", expo->key, expo->value);
-    }
 }
-
-// void ft_backup(t_env **head, t_minishell data)
-// {
-//     t_env *current = *head;
-//     t_env *prev = NULL;
-
-//     if (data.tokens == NULL || data.tokens->next_token == NULL)
-//         return;
-
-//     while (current != NULL)
-//     {
-//         current->test = data.tokens->next_token->data;  // Token containing the key to unset
-
-//         // If the key matches the token, unset it
-//         if (current->test != NULL && !ft_strcmp(current->key, current->test))
-//         {
-//             printf("\033[36m Unsetting key: %s \033[0m\n", current->key);
-//             // printf("%s<------------", current->key);
-//             free(current->key);
-//             free(current->value);
-
-//             // Unlink the node from the list
-//             if (prev == NULL) 
-//             {
-//                 // If current node is the head
-//                 *head = current->next;
-//             } 
-//             else 
-//             {
-//                 // Skip the current node
-//                 prev->next = current->next;
-//             }
-
-//             free(current);  // Free the node memory
-//             current = (prev == NULL) ? *head : prev->next;  // Move the current pointer forward
-//             continue;  // Skip to the next iteration after deletion
-//         }
-
-//         // If the key wasn't unset, proceed to the next node
-//         prev = current;
-//         current = current->next;
-//     printf("%s<------------", current->key);
-//     }
-//     printf("outloop %s s<------------", current->key);
-// }
-
-// void ft_env(t_minishell data)
-// {
-//     int i = 0;
-//     t_env *head = NULL;
-//     t_env *last_node = NULL;
-    
-//     while(data.envirement[i] != NULL)
-//     {
-//         t_env *cmd_env = (t_env *)malloc(sizeof(t_env));
-//         if (!cmd_env)
-//             exit(1);
-//         char *equal_env = strchr(data.envirement[i], '=');
-//         if (equal_env != NULL)
-//         {
-//             cmd_env->key = strndup(data.envirement[i], equal_env - data.envirement[i]);
-//             cmd_env->value = strndup(equal_env + 1, ft_strlen(data.envirement[i] + 1 ));
-// 			cmd_env->equal = '=';
-// 			cmd_env->next = NULL;
-//             printf("%s", cmd_env->key);
-//             printf("%c", cmd_env->equal);
-//             printf("%s\n", cmd_env->value);
-//         }
-//         if( last_node == NULL)
-//             head = cmd_env;
-//         else
-//             last_node->next = cmd_env;
-//         last_node = cmd_env;
-//         i++;
-//     }
-// }
